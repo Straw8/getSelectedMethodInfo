d09e2cb3118e4eb90d67999a1340ec23892b17c1
DavidLin <546035844@qq.com>
2018-12-18 16:28:17 +0800

    优化代码

--- src/com/asiainfo/unittest/RecordUtCfgAction.java
+++ src/com/asiainfo/unittest/RecordUtCfgAction.java
@@ -7,6 +7,7 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.psi.*;
+import org.apache.http.util.TextUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,35 +19,36 @@ public class RecordUtCfgAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         PsiMethod selectedMethod = this.getSelectedMethod(e);
-        if (selectedMethod != null){
+        if (selectedMethod != null) {
             HashMap<String, Object> methodInfo = this.getMethodInfo(selectedMethod);
-            System.out.println(1);
         }
     }
 
-    private PsiMethod getSelectedMethod(AnActionEvent e){
+    private PsiMethod getSelectedMethod(AnActionEvent e) {
         PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
         Editor editor = e.getData(LangDataKeys.EDITOR);
-        SelectionModel selectionModel = editor.getSelectionModel();
-        if (!"".equals(selectionModel.getSelectedText())) {
-            String selectedText = selectionModel.getSelectedText();
-            FileType fileType = psiFile.getFileType();
-            if (FILE_TYPE.equals(fileType.getName()) && psiFile instanceof PsiJavaFile) {
-                PsiJavaFile javaFile = (PsiJavaFile) psiFile;
-                String fileName = javaFile.getName();
-                String className = fileName.substring(0, fileName.length() - 5);
-                PsiClass[] psiClasses = javaFile.getClasses();
-                PsiClass currentClass = null;
-                for (PsiClass psiClass : psiClasses) {
-                    if (className.equals(psiClass.getName())) {
-                        currentClass = psiClass;
-                        break;
+        if (editor != null) {
+            SelectionModel selectionModel = editor.getSelectionModel();
+            if (!TextUtils.isEmpty(selectionModel.getSelectedText())) {
+                String selectedText = selectionModel.getSelectedText();
+                FileType fileType = psiFile.getFileType();
+                if (FILE_TYPE.equals(fileType.getName()) && psiFile instanceof PsiJavaFile) {
+                    PsiJavaFile javaFile = (PsiJavaFile) psiFile;
+                    String fileName = javaFile.getName();
+                    String className = fileName.substring(0, fileName.length() - 5);
+                    PsiClass[] psiClasses = javaFile.getClasses();
+                    PsiClass currentClass = null;
+                    for (PsiClass psiClass : psiClasses) {
+                        if (className.equals(psiClass.getName())) {
+                            currentClass = psiClass;
+                            break;
+                        }
                     }
-                }
-                PsiMethod[] allMethods = currentClass.getAllMethods();
-                for (PsiMethod method : allMethods) {
-                    if (method.getName().equals(selectedText)) {
-                        return method;
+                    PsiMethod[] allMethods = currentClass.getAllMethods();
+                    for (PsiMethod method : allMethods) {
+                        if (method.getName().equals(selectedText)) {
+                            return method;
+                        }
                     }
                 }
             }
@@ -54,18 +56,18 @@ public class RecordUtCfgAction extends AnAction {
         return null;
     }
 
-    private HashMap<String,Object> getMethodInfo(PsiMethod selectedMethod){
-        HashMap<String,Object> map = new HashMap<>();
-        map.put("methodName",selectedMethod.getName());
+    private HashMap<String, Object> getMethodInfo(PsiMethod selectedMethod) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("methodName", selectedMethod.getName());
         PsiParameterList parameterList = selectedMethod.getParameterList();
-        Map<String,String> paramMap = new HashMap<>();
+        Map<String, String> paramMap = new HashMap<>();
         PsiParameter[] parameters = parameterList.getParameters();
-        for (PsiParameter p : parameters){
-            paramMap.put(p.getType().getPresentableText(),p.getName());
+        for (PsiParameter p : parameters) {
+            paramMap.put(p.getType().getPresentableText(), p.getName());
             System.out.println(1);
         }
-        map.put("params",paramMap);
-        map.put("returnType",selectedMethod.getReturnType().getPresentableText());
+        map.put("params", paramMap);
+        map.put("returnType", selectedMethod.getReturnType().getPresentableText());
         return map;
     }
 }
